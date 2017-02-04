15e199acbc8580a147d0d579c1e808f68d5daa8b
mrkiffie <mrkiffie@gmail.com>
2017-02-04 19:57:39 +0200

    Add answers to a cooldown queue

--- src/data/quiz.js
+++ src/data/quiz.js
@@ -3,9 +3,19 @@ import countries from '../data/countries';
 
 export const QUIZ_OPTIONS_COUNT = 5;
 
+let queue = [];
+
 export const generateQuizOptions = (num = QUIZ_OPTIONS_COUNT) => {
-  const options = Random.getRandomRange(countries, num);
+
+  if (queue.length > countries.length * .2) {
+    queue = queue.slice(1);
+  }
+
+  const options = Random.getRandomRange(countries.filter(country => queue.indexOf(country.iso3) === -1), num);
   const answer = Random.getRandomRange(options, 1)[0];
+
+  queue.push(answer.iso3);
+
   return {
     options,
     answer
